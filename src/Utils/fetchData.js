4180fdce9f3f18ed380d5c31e0ac7db68bec16c1
Mason Wester <masonwester21@gmail.com>
2025-08-09 20:12:30 -0500

    Added Exercises Details and Similar Exercises

--- src/Utils/fetchData.js
+++ src/Utils/fetchData.js
@@ -1,24 +1,22 @@
-
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-  'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-  'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-}
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
+  },
 };
 
 export const fetchData = async (url, options) => {
-  const response = await fetch(url, options);
-  let data;
-  try {
-    data = await response.json();
-  } catch (e) {
-    console.error('Failed to parse JSON:', e);
-    data = null;
-  }
-  console.log('RAW API response:', data);
-  if (!Array.isArray(data)) {
-    console.error('API response is not an array:', data);
-  }
+  const res = await fetch(url, options);
+  const data = await res.json();
+
   return data;
 };
